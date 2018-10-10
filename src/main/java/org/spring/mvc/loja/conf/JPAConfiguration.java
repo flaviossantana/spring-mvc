911fcd62e68238ad0e327e32f96d6588f785dcea
flavio.santana <9090flavio@gmail.com>
2018-10-09 23:57:13 -0300

    Preparando ProdutosController

--- src/main/java/org/spring/mvc/loja/conf/JPAConfiguration.java
+++ src/main/java/org/spring/mvc/loja/conf/JPAConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import java.util.Properties;
 
